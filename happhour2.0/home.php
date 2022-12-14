b9ddc03c4131940e5f435d93fcdaee8a7148b27b
Jesswon <jessica.won.75@my.csun.edu>
2022-12-13 21:50:48 -0800

    update carousel

--- happhour2.0/home.php
+++ happhour2.0/home.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
     <title>My PHP page</title>
     <style>
         /* Basic styles for the page */
@@ -23,19 +24,109 @@
             margin: 0;
         }
 
-        /*centers images jw*/ 
+        /*centers images jw 
         .logo {
             display: flex;
             align-items: center;
             justify-content: center;
-            color: #111; 
-            background: greenyellow; 
-            box-shadow: 0 0 100px greenyellow;
             margin-top: 20px;
             margin-bottom: 20px;
         }
+        */
+
+        * {box-sizing: border-box}
+        body {font-family: Verdana, sans-serif; margin:0}
+        .mySlides {display: none}
+        img {vertical-align: middle;}
+
+        /* Slideshow container */
+        .slideshow-container {
+        max-width: 1000px;
+        position: relative;
+        margin: auto;
+        }
+
+        /* Next & previous buttons */
+        .prev, .next {
+        cursor: pointer;
+        position: absolute;
+        top: 50%;
+        width: auto;
+        padding: 16px;
+        margin-top: -22px;
+        color: white;
+        font-weight: bold;
+        font-size: 18px;
+        transition: 0.6s ease;
+        border-radius: 0 3px 3px 0;
+        user-select: none;
+        }
+
+        /* Position the "next button" to the right */
+        .next {
+        right: 0;
+        border-radius: 3px 0 0 3px;
+        }
+
+        /* On hover, add a black background color with a little bit see-through */
+        .prev:hover, .next:hover {
+        background-color: rgba(0,0,0,0.8);
+        }
+
+        /* Caption text */
+        .text {
+        color: #f2f2f2;
+        font-size: 15px;
+        padding: 8px 12px;
+        position: absolute;
+        bottom: 8px;
+        width: 100%;
+        text-align: center;
+        }
+
+        /* Number text (1/3 etc) */
+        .numbertext {
+        color: #f2f2f2;
+        font-size: 12px;
+        padding: 8px 12px;
+        position: absolute;
+        top: 0;
+        }
+
+        /* The dots/bullets/indicators */
+        .dot {
+        cursor: pointer;
+        height: 15px;
+        width: 15px;
+        margin: 0 2px;
+        background-color: #bbb;
+        border-radius: 50%;
+        display: inline-block;
+        transition: background-color 0.6s ease;
+        }
+
+        .active, .dot:hover {
+        background-color: #717171;
+        }
+
+        /* Fading animation */
+        .fade {
+        animation-name: fade;
+        animation-duration: 1.5s;
+        }
+
+        @keyframes fade {
+        from {opacity: .4} 
+        to {opacity: 1}
+        }
+
+        /* On smaller screens, decrease text size */
+        @media only screen and (max-width: 300px) {
+        .prev, .next,.text {font-size: 11px}
+        }
 
     </style>
+
     <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
     <script >
         function fav(restaurantid,userid){
@@ -61,19 +152,80 @@
             });
             //some code
         }
+
+        //PICTURE CAROUSEL jw
+        let slideIndex = 1;
+        showSlides(slideIndex);
+
+        function plusSlides(n) {
+        showSlides(slideIndex += n);
+        }
+
+        function currentSlide(n) {
+        showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {slideIndex = 1}    
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+        }
+        for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " active";
+        }
+
     </script>
 </head>
 <body>
-<nav>
-        <a href="./home.php">Home</a>
-        <a href="./favoritesPage.php">Favorites</a>
-        <a href="./logout.php">logOut</a>
+    <nav>
+        <!--HAPPY HOUR LOGO jw-->
+        <img src="./happyhourlogo.png" width='100px' height='100px' alt='image'>
+        <a href="./home.php">HOME</a>
+        <a href="./favoritesPage.php">FAVORITES</a>
+        <a href="./logout.php">LOGOUT</a>
     </nav>
 
-    <!--HAPPY HOUR LOGO jw-->
-    <div class="logo">
-    <img src="./happyhourlogo.png" width='300px' height='300px' alt='image'>
+    <!--PICTURE CAROUSEL jw-->
+    <div class="slideshow-container">
+
+    <div class="mySlides fade">
+    <div class="numbertext">1 / 3</div>
+    <img src="./hamburger.jpg" style="width:100%">
+    <div class="text">Caption Text</div>
     </div>
+
+    <div class="mySlides fade">
+    <div class="numbertext">2 / 3</div>
+    <img src="./pizza.jpg" style="width:100%">
+    <div class="text">Caption Two</div>
+    </div>
+
+    <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img src="./wine.jpg" style="width:100%">
+    <div class="text">Caption Three</div>
+    </div>
+
+    <a class="prev" onclick="plusSlides(-1)">❮</a>
+    <a class="next" onclick="plusSlides(1)">❯</a>
+
+    </div>
+    <br>
+
+    <div style="text-align:center">
+    <span class="dot" onclick="currentSlide(1)"></span> 
+    <span class="dot" onclick="currentSlide(2)"></span> 
+    <span class="dot" onclick="currentSlide(3)"></span> 
+    </div>
+
+
 
     <main>
         <!-- PHP code that retrieves data from the database and displays it in a table -->
