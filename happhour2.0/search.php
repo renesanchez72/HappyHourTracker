80e092ee8ed6049ad73e01d48dd455f6bd114c2d
renesanchez72 <rene.sanchez.72@my.csun.edu>
2023-05-18 01:40:22 -0700

    test

--- happhour2.0/search.php
+++ happhour2.0/search.php
@@ -11,6 +11,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="search.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
     <script>
         function fav(restaurantid,userid){
