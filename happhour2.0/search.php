68c4f6383a3a87cf3098483f686179fa7173e8a0
renesanchez72 <rene.sanchez.72@my.csun.edu>
2023-05-18 11:21:17 -0700

    regular text

--- happhour2.0/search.php
+++ happhour2.0/search.php
@@ -191,7 +191,7 @@
               echo "<div class='info'>";
               echo "<div class='name'>";
               // echo "$rest_name";
-              echo "<a href='https://www.yelp.com/search?find_desc=$rest_name' target='_blank'><p class='name'>$rest_name</p></a>";
+              echo "<a href='https://www.yelp.com/search?find_desc=$rest_name' target='_blank'><p >$rest_name</p></a>";
             echo "</div>";
               echo "<div class='line'></div>";
               echo "<div class='description'>";
